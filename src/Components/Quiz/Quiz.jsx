055ae44e2719ca22b3f3c7acba4bbfddce018307
amank010 <amanksingh010@gmail.com>
2025-04-27 00:21:16 +0530

    1st Complete Draft

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -5,6 +5,10 @@ const Quiz = () => {
     let [index,setIndex] = useState(0);
     let [question,setQuestion] = useState(data[index]);
     let[lock,setLock] = useState(false);
+    let[score,setScore] = useState(0);
+    let[incorrect,setIncorrect] = useState(0);
+    let[result,setResult] = useState(false);
+    let[userAnswers,setUserAnswers]=useState({});
 
     let Option1=useRef(null);
     let Option2=useRef(null);
@@ -17,20 +21,103 @@ const Quiz = () => {
         if(lock==false){
             if(question.ans==ans){
                 e.target.classList.add("correct");
-                setLock(true);
+                
+                setScore(prev=>prev+1);
             }
             else{
                 e.target.classList.add("wrong");
-                setLock(true);
+                
                 option_array[question.ans-1].current.classList.add("correct");
+                setIncorrect(prev=>prev+1);
             }
+            setLock(true);
+            //save
+            setUserAnswers(prev=>({
+              ...prev,
+              [index]:ans
+            }));
         }
 
     }
+    
+    const next=()=>{
+      if(index==data.length-1){
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(data[index]);
+
+      // setLock(false);
+      //clear old classes first
+      option_array.map((option)=>{
+        option.current.classList.remove("wrong");
+        option.current.classList.remove("correct");
+        // return null;
+      });
+      //if answered already
+      if(userAnswers[index]!=undefined){
+        const selectedOption=userAnswers[index];
+
+        if(selectedOption==data[index].ans){
+          option_array[selectedOption-1].current.classList.add("correct");
+        }
+        else{
+          option_array[selectedOption - 1].current.classList.add("wrong");
+          option_array[data[index].ans - 1].current.classList.add("correct");
+        }
+        setLock(true);
+      }
+      else{
+        setLock(false);
+      }
+
+    }
+
+    const prev=()=>{
+      if(index==0){
+         
+        return 0;
+      }      
+      setIndex(--index);
+      setQuestion(data[index]);
+      // setLock(false);
+      option_array.map((option)=>{
+        option.current.classList.remove("wrong");
+        option.current.classList.remove("correct");
+        return null;
+      });
+
+      //store
+      if (userAnswers[index] != undefined) {
+        const selectedOption = userAnswers[index];
+    
+        if (selectedOption == data[index].ans) {
+          option_array[selectedOption - 1].current.classList.add("correct");
+        } else {
+          option_array[selectedOption - 1].current.classList.add("wrong");
+          option_array[data[index].ans - 1].current.classList.add("correct");
+        }
+        setLock(true);
+      } else {
+        setLock(false);
+      }
+      
+    }
+    const reset=()=>{
+      setIndex(0);
+      setQuestion(data[0]);
+      setScore(0);
+      setLock(false);
+      setResult(false);
+    }
+
   return (
+
     <div className='container'>
       <h1>QuizApp</h1>
       <hr />
+      {result?<></>:<>
       <h2>{index+1}. {question.question}</h2>
     <ul>
         <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
@@ -38,8 +125,18 @@ const Quiz = () => {
         <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3} </li>
         <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}> {question.option4}</li>
     </ul>
-    <button>Next</button>
-    <div className="index">1 of 5 questions</div>
+    <button onClick={next}>Next</button>
+    <button onClick={prev}>Previous</button>
+    <div className="index">{index+1} of {data.length} questions</div>
+      </>}
+      {result?<>
+        <div className='score-section'>
+        <h2>Correct Answers: {score}</h2>
+        <h2>Incorrect Answers: {incorrect}</h2>
+      </div>
+      <h2>Total Questions: {data.length}</h2>
+      <button onClick={reset}>Reset</button>
+      </>:<></>}
     </div>
   )
 }
